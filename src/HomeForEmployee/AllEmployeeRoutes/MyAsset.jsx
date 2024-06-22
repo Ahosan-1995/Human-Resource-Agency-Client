0d6867971fd1b299bd8d5229d71a65b6e2ed64cd
Ahosan-1995 <ahsoanhabibshakil2@gmail.com>
2024-06-22 14:53:32 +0600

    my team has benn done

--- src/HomeForEmployee/AllEmployeeRoutes/MyAsset.jsx
+++ src/HomeForEmployee/AllEmployeeRoutes/MyAsset.jsx
@@ -2,12 +2,67 @@ import { Helmet } from "react-helmet-async";
 import OnlyUsersReload from "../../Hooks/OnlyUsersReload";
 import OnlyRequestedAsset from "../../Hooks/OnlyRequestedAsset";
 
+import React, { useState } from "react";
+import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
+
+// For modal
+
+import Modal from "react-modal";
+import { useForm } from "react-hook-form";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+// Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement(document.getElementById("root"));
+// For modal
+
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: "row",
+    backgroundColor: "#E4E4E4",
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1,
+  },
+});
+
 const MyAsset = () => {
   const [requestedAssets, loading, refetch] = OnlyRequestedAsset();
   console.log(requestedAssets);
+  const { register, handleSubmit, reset } = useForm();
 
   const [allUsers] = OnlyUsersReload();
   // console.log(allUsers);
+  // For modal
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = "#f00";
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+  const [selectedAsset, setSelectedAsset] = useState(null);
+  // For modal
+
+  const handlePrint = () => {};
 
   return (
     <div>
@@ -38,27 +93,75 @@ const MyAsset = () => {
               </tr>
             </thead>
             <tbody>
-              {requestedAssets.map((asset,index) => (
+              {requestedAssets.map((asset, index) => (
                 <tr key={asset._id}>
-                  <th>{index+1}</th>
+                  <th>{index + 1}</th>
                   <th>{asset.productName}</th>
                   <th>{asset.productType}</th>
                   <th>{asset.requestDate}</th>
-                  <th>{asset.status==='pending'?'': asset.approveDate}</th>
+                  <th>{asset.status === "pending" ? "" : asset.approveDate}</th>
                   <th>{asset.status}</th>
                   <th>
-                    {
-                      asset.approveDate?<button disabled className="btn">Remove</button>:<button className="btn">Remove</button>
-                    }
+                    {asset.approveDate ? (
+                      <button disabled className="btn">
+                        Remove
+                      </button>
+                    ) : (
+                      <button className="btn">Remove</button>
+                    )}
                   </th>
-                  {
-                    asset.status==='approved'?
-                    <th>
-                    Print
-                  </th>
-                  :
-                  ''
-                  }
+                  {asset.status === "approved" ? (
+                    <div>
+                      <button
+                        onClick={() => {
+                          setSelectedAsset(asset);
+                          openModal();
+                        }}
+                        className="btn"
+                      >
+                        Print
+                      </button>
+                      <Modal
+                        isOpen={modalIsOpen}
+                        onAfterOpen={afterOpenModal}
+                        onRequestClose={closeModal}
+                        style={customStyles}
+                        contentLabel="Example Modal"
+                      >
+                        <div className="flex justify-between">
+                          <h2 ref={(_subtitle) => (subtitle = _subtitle)}>
+                            Additional Note:
+                          </h2>
+                          <button
+                            className="w-12 rounded-md  bg-slate-300"
+                            onClick={closeModal}
+                          >
+                            close
+                          </button>
+                        </div>
+
+                        <form onSubmit={handleSubmit(handlePrint)}>
+                          <Document>
+                            <Page size="A4" style={styles.page}>
+                              <View style={styles.section}>
+                                <div>
+                                  <p>Product Name:{asset.productName}</p>
+                                  <p>Product Type:{asset.productType}</p>
+                                  <p>Approve Date:{asset.requestDate}</p>
+                                  <p>Approver: {asset.approverEmail}</p>
+                                </div>
+                              </View>
+                            </Page>
+                          </Document>
+                          <button type="submit" className="btn">
+                            Print
+                          </button>
+                        </form>
+                      </Modal>
+                    </div>
+                  ) : (
+                    ""
+                  )}
                 </tr>
               ))}
             </tbody>
@@ -70,17 +173,3 @@ const MyAsset = () => {
 };
 
 export default MyAsset;
-
-{
-  /* <tr>
-  <th>Sl</th>
-  <th>Asset name</th>
-  <th>Asset type</th>
-  <th>Request date</th>
-  <th>Request status</th>
-  <th>Approval date</th>
-  <th>
-    <button className="btn">Remove</button>
-  </th>
-</tr> */
-}
--- src/HomeForEmployee/AllEmployeeRoutes/MyTeam.jsx
+++ src/HomeForEmployee/AllEmployeeRoutes/MyTeam.jsx
@@ -1,6 +1,45 @@
 import { Helmet } from "react-helmet-async";
+import OnlyUsersReload from "../../Hooks/OnlyUsersReload";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
+
+
+
 
 const MyTeam = () => {
+  const [allUsers,loading,refetch]=OnlyUsersReload();
+
+  const { user } = useContext(AuthContext);
+
+  const userEmail=user.email;
+
+  const [filteredUsers, setFilteredUsers] = useState([]);
+
+  const [filteredUserAssociatedEmail, setfilteredUserAssociatedEmail] = useState([]);
+
+  useEffect(() => {
+    if (allUsers.length > 0) {
+      const filtered = allUsers.filter(user => user.email === userEmail);
+      setFilteredUsers(filtered);
+    }
+  }, [allUsers,user]);
+
+  // console.log(filteredUsers);
+
+  // const filteredUserAssociatedEmail=filteredUsers[0]?.associatedEmail;
+  // console.log(filteredUserAssociatedEmail);
+
+  useEffect(() => {
+    // Log filteredUsers whenever it changes
+    console.log(filteredUsers);
+  
+    // Log filteredUserAssociatedEmail whenever it changes
+    const filteredUserAssociatedEmail = filteredUsers[0]?.associatedEmail;
+    setfilteredUserAssociatedEmail(filteredUserAssociatedEmail);
+  }, [filteredUsers]);
+
+
+
   return (
     <div>
       <Helmet>
@@ -20,12 +59,27 @@ const MyTeam = () => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th>Sl</th>
-              <th>Image of the member</th>
-              <th>Name of the member</th>
-              <th>Member type</th>
-            </tr>
+            {
+              allUsers.map((user,index)=>
+              filteredUserAssociatedEmail==user.associatedEmail || filteredUserAssociatedEmail==user.email?
+              <tr key={user._id}>
+                <th>{index+1}</th>
+                <th>
+                    <img
+                      className="w-10 rounded-full"
+                      alt="Img"
+                      src="https://i.ibb.co/ygVzCDL/128-1280406-view-user-icon-png-user-circle-icon-png.png"
+                    />
+                  </th>
+                <th>{user.name}</th>
+                <th>{user.role}</th>
+              </tr>:
+              ''
+              
+              
+              )
+            }
+            
           </tbody>
         </table>
       </div>
